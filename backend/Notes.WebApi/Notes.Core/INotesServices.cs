71b9d277797d7492f321b6d390e9481cf1957caf
Jeremiah <jeremiahteee@gmail.com>
2022-02-01 15:36:37 -0500

    backend in ASP.NET core

---
+++ backend/Notes.WebApi/Notes.Core/INotesServices.cs
@@ -0,0 +1,13 @@
+﻿using Notes.DB;
+
+namespace Notes.Core
+{
+    public interface INotesServices
+    {
+        Note CreateNote(Note note);
+        Note GetNote(int id);
+        List<Note> GetNotes();
+        void DeleteNote(int id);
+        void EditNote(Note note);
+    }
+}
---
+++ backend/Notes.WebApi/Notes.Core/NotesServices.cs
@@ -0,0 +1,45 @@
+﻿using Notes.DB;
+
+namespace Notes.Core
+{
+    public class NotesServices : INotesServices
+    {
+        private AppDbContext _dbContext;
+        public NotesServices(AppDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public Note CreateNote(Note note)
+        {
+            _dbContext.Add(note);
+            _dbContext.SaveChanges();
+
+            return note;
+        }
+
+        public Note GetNote(int id)
+        {
+            return _dbContext.Notes.First(n => n.Id == id);
+        }
+
+        public List<Note> GetNotes()
+        {
+            return _dbContext.Notes.ToList();
+        }
+
+        public void DeleteNote(int id)
+        {
+            var note = _dbContext.Notes.First(n => n.Id == id);
+            _dbContext.Notes.Remove(note);
+            _dbContext.SaveChanges();
+        }
+
+        public void EditNote(Note note)
+        {
+            var editedNote = _dbContext.Notes.First(n => n.Id == note.Id);
+            editedNote.Value = note.Value;
+            _dbContext.SaveChanges();
+        }
+    }
+}
